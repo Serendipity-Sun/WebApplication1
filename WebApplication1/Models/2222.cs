d80b371e12a84d6c8bb4612ef4e7f7945bf23400
s a t <s a t@SERENDIPITY>
2020-11-11 15:16:41 +0800

    一个类

---
+++ WebApplication1/Models/2222.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class _2222
+    {
+        public int MyProperty { get; set; }
+    }
+}
